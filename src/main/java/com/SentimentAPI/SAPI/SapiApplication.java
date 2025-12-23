d073da617769ff3a667efa10908e66054a858401
codigobenja <benjacorp94@gmail.com>
2025-12-22 22:05:07 -0700

    Initial commit

---
+++ src/main/java/com/SentimentAPI/SAPI/SapiApplication.java
@@ -0,0 +1,13 @@
+package com.SentimentAPI.SAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SentimentAPI/SAPI/SapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SentimentAPI.SAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
