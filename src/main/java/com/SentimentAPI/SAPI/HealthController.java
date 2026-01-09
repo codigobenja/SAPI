c02950c15c42db42a63e18a94575664546592947
codigobenja <benjacorp94@gmail.com>
2026-01-08 17:34:27 -0700

    correcciones 502

---
+++ src/main/java/com/SentimentAPI/SAPI/HealthController.java
@@ -0,0 +1,13 @@
+package com.SentimentAPI.SAPI;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+	@GetMapping("/")
+	public String home() {
+		return "Sentiment API running";
+	}
+}
